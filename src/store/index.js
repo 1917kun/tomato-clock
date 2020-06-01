f0a1f1f76d747636e342652dce9e010b4ff9430f
1917kun <anleader911@gmail.com>
2020-06-01 09:25:37 +0800

    add file

---
+++ src/store/index.js
@@ -0,0 +1,86 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+
+Vue.use(Vuex)
+
+const timeleft = parseInt(process.env.VUE_APP_TIMELEFT)
+const timeleftBreak = parseInt(process.env.VUE_APP_TIMELEFT_BREAK)
+export default new Vuex.Store({
+  state: {
+    todos: [],
+    timeleft,
+    alarm: 'alarm1.mp3',
+    current: '',
+    isBreak: false
+  },
+  mutations: {
+    selectAlarm (state, data) {
+      state.alarm = data
+    },
+    addTodo (state, data) {
+      state.todos.push({ name: data, edit: false, model: data })
+    },
+    dragTodo (state, data) {
+      // data = data.filter(d => {
+      //   return d !== undefined
+      // })
+      state.todos = data
+    },
+    delTodo (state, data) {
+      state.todos.splice(data, 1)
+    },
+    editTodo (state, data) {
+      state.todos[data].edit = !state.todos[data].edit
+    },
+    cancelTodo (state, data) {
+      state.todos[data].edit = false
+      state.todos[data].model = state.todos[data].name
+    },
+    saveTodo (state, data) {
+      state.todos[data].edit = false
+      state.todos[data].name = state.todos[data].model
+    },
+    // 如果是休息中我就倒數五分鐘 如果不是休息中就倒數25分鐘
+    start (state) {
+      if (state.isBreak) {
+        state.current = '休息一下'
+      } else if (!state.isBreak) {
+        state.current = state.todos[0].name
+        state.todos.splice(0, 1)
+      }
+    },
+    countdown (state) {
+      state.timeleft--
+    },
+    finish (state) {
+      if (state.todos.length > 0) {
+        state.isBreak = !state.isBreak
+      }
+      state.current = ''
+      state.timeleft = state.isbreak ? timeleftBreak : timeleft
+    }
+  },
+  actions: {
+  },
+  getters: {
+    alarm (state) {
+      return state.alarm
+    },
+    todos (state) {
+      return state.todos
+    },
+    timeleft (state) {
+      return state.timeleft
+    },
+    current (state) {
+      return state.current
+    },
+    isBreak (state) {
+      return state.isBreak
+    }
+  },
+  modules: {
+  },
+  plugins: [createPersistedState()]
+})
---
+++ src/views/Settings.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="settings">
+    <b-table :items="items" :fields="fields" @row-clicked="selectAlarm">
+      <template v-slot:cell(preview)="data">
+        <!-- {{data}} 用來檢查 -->
+        <audio controls :src="'./alarms/'+data.item.file"></audio>
+      </template>
+      <template v-slot:cell(select)="data">
+        <font-awesome-icon v-if="data.item.file==alarm" :icon="['fas','check']"></font-awesome-icon>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          name: '鬧鈴',
+          file: 'alarm1.mp3'
+        },
+        {
+          name: 'alarm2',
+          file: 'alarm2.mp3'
+        },
+        {
+          name: 'ya',
+          file: 'ya.mp3'
+        }
+      ],
+      fields: [
+        {
+          key: 'name',
+          label: '名稱'
+        },
+        {
+          key: 'preview',
+          label: '預覽'
+        },
+        {
+          key: 'select',
+          label: '選擇'
+        }
+      ]
+    }
+  },
+  computed: {
+    alarm () {
+      return this.$store.getters.alarm
+    }
+  },
+  methods: {
+    selectAlarm (item) {
+      this.$store.commit('selectAlarm', item.file)
+    }
+  }
+}
+</script>
